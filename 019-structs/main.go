7550e249e5a74e98a7c11cc0a8f486128f6a18ad
matty-cakes <matt.cale@hey.com>
2025-01-27 10:31:12 -0600

    feat: mods to lesson 19

--- 019-structs/main.go
+++ 019-structs/main.go
@@ -24,12 +24,12 @@ func main() {
 
 	fmt.Println("-- We can create a new person like so:")
 	walter := person{name: "walter", age: 22}
-	fmt.Println("-- walter := person{name: \"walter\", age: 22}")
+	fmt.Println("-- walter := person{name: \"walter\", age: 22} --", walter)
 	fmt.Println("-- We can use the dot char '.' to access attributes of structs. Ex: 'walter.age':", walter.age)
 
 	fmt.Println("-- You can create a struct, get it's pointer value AND use the same dot syntax on the pointer. The pointer is magically derefenced for you:")
 	sam := &person{name: "sam", age: 44}
-	fmt.Println("-- sam := &person{name: \"sam\", age: 44}")
+	fmt.Println("-- sam := &person{name: \"sam\", age: 44} --", sam)
 	fmt.Println("-- What is sam's age: 'same.age'", sam.age)
 
 	fmt.Println("-- Lastly, you can create inline/anonymous structs for one time uses:")
